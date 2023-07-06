0096c041d7eb76934311c5ef09788bb988dc62dc
BuildTools <unconfigured@null.spigotmc.org>
2023-07-06 14:19:42 -0300

    Salvando minha primeira tela em outro arquivo "minha_primeira_tela" e começando a estudar mais afundo sobre manipulação de widgets e criação de widgets personalizados para criar um app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,186 +17,77 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: true,
+        // primarySwatch: Colors.green, //brinks
       ),
-      home: Container(
-        color: Colors.blueGrey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aprendendo mais sobre Scaffold")
+        ),
+            body: ListView(
               children: [
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
+                Task("Global no CS",),
+                Task("Chorar no b33333333anho"),
+                Task("Roubar um banc333333333333333333333333333o"),
+                Task("Andar de bike"),
+                Task("Roubar um banc33333333333333333333333333333333333333333333333o"),
+                Task("Roubar um banco"),
+                Task("Roubar um ban333co"),
+                Task("Roubar um banco"),
+                Task("Roubar um banco"),
+                Task("Roubar um banco"),
+                Task("Roubar um banco"),
               ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(color: Colors.redAccent,width: 120,height: 120),
-                    Container(color: Colors.white, width: 110,height: 110),
-                    Container(color: Colors.green, width: 100,height: 100),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                 Container(
-                  color: Colors.amber,
-                  height: 30,
-                  width: 200,
-                  child: Text(
-                    "Ola, Mundo",
-                    style: TextStyle(color:Colors.deepPurple,fontSize: 29),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                ElevatedButton(onPressed: (){print("Voce apertou algo");}, child: Text("Aperta Isso"))
-              ],
-            ),
+            ), //brinks
+            floatingActionButton: FloatingActionButton(onPressed: (){},),
+      ),
+      );
+  }
+}
 
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome,{Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Container(
+                        width: 200,
+                        child: Text(
+                          nome,
+                          style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis),
+                          )
+                    ),
+                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                )
+            )
           ],
         ),
       ),
-      );
+    );
   }
 }
