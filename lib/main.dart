741e4be5838483d049f16d728be349ae88822767
BuildTools <unconfigured@null.spigotmc.org>
2023-07-07 17:47:18 -0300

    Deixando o codigo mais limpo: Criando uma arquivo separado para o sistema de dificuldade das Tasks.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'difficulty.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -119,16 +121,7 @@ class _TaskState extends State<Task> {
                                       fontSize: 24,
                                       overflow: TextOverflow.ellipsis),
                                 ),
-                                Row(
-                                children: [
-                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=1)?Colors.blue:Colors.blue[100],),
-                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=2)?Colors.blue:Colors.blue[100],),
-                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=3)?Colors.blue:Colors.blue[100],),
-                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=4)?Colors.blue:Colors.blue[100],),
-                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=5)?Colors.blue:Colors.blue[100],),
-
-                                ]
-                              ),
+                                Difficulty(difficultyLevel: widget.dificuldade,),
                             ],
                             ),
                         ),
