5f71af179bf148b5354d156f3e414d6e37e9e103
BuildTools <unconfigured@null.spigotmc.org>
2023-07-07 17:23:30 -0300

    Organizando melhor o codigo, separando antes o codigo comentado agora em um novo arquivo para usar caso queira analizar meu primeiro codigo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,70 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        // useMaterial3: true,
-        // primarySwatch: Colors.green, //brinks
-      ),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          // useMaterial3: true,
+          // primarySwatch: Colors.green, //brinks
+          ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Aprendendo mais sobre Scaffold")
+        appBar: AppBar(leading: Container(), title: Text("Tarefas")),
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(milliseconds: 800),
+          child: ListView(
+            children: [
+              Task("Criar Virus Mobile","https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",5),
+              Task("Jogar a Bike Fora","https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg",1),
+              Task("Aprender Magia","https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg",3),
+              Task("Elevar meu Chakra","https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg",4),
+              Task("Jogar Mario 2","https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg",2),
+
+            ],
+          ),
+        ), //brinks
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+          child: Icon(Icons.remove_red_eye_sharp),
         ),
-            body: ListView(
-              children: [
-                Task("Global no CS",),
-                Task("Chorar no b33333333anho"),
-                Task("Roubar um banc333333333333333333333333333o"),
-                Task("Andar de bike"),
-                Task("Roubar um banc33333333333333333333333333333333333333333333333o"),
-                Task("Roubar um banco"),
-                Task("Roubar um ban333co"),
-                Task("Roubar um banco"),
-                Task("Roubar um banco"),
-                Task("Roubar um banco"),
-                Task("Roubar um banco"),
-              ],
-            ), //brinks
-            floatingActionButton: FloatingActionButton(onPressed: (){},),
       ),
-      );
+    );
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
-  const Task(this.nome,{Key? key}) : super(key: key);
+  final String foto;
+  final int dificuldade;
+
+  const Task(this.nome,this.foto,this.dificuldade, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -57,33 +73,115 @@ class Task extends StatelessWidget {
       child: Container(
         child: Stack(
           children: [
-            Container(
-              color: Colors.blue,
-              height: 140,
+            Column(
+              children: [
+                Container(
+                  height: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
             ),
-            Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
+            Column(
+              children: [
+                Container(
+                    color: Colors.white,
+                    height: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(2),
+                            color: Colors.black26,
+                          ),
+                          width: 72,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(2),
+                            child: Image.network(
+                                widget.foto,
+                              fit:  BoxFit.cover,
+                            ),
+                          ),
+                        ), 
+                        Container(
+                            width: 200,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.nome,
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                                Row(
+                                children: [
+                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=1)?Colors.blue:Colors.blue[100],),
+                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=2)?Colors.blue:Colors.blue[100],),
+                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=3)?Colors.blue:Colors.blue[100],),
+                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=4)?Colors.blue:Colors.blue[100],),
+                                  Icon(Icons.star, size: 15,color: (widget.dificuldade>=5)?Colors.blue:Colors.blue[100],),
+
+                                ]
+                              ),
+                            ],
+                            ),
+                        ),
+                        Container(
+                          height: 52,
+                          width: 52,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  nivel++;
+                                });
+                                print(nivel);
+                              },
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Icon(Icons.arrow_drop_up),
+                                  Text(
+                                    "UP",
+                                    style: TextStyle(fontSize: 10),
+                                  ),
+                                ],
+                              )),
+                        )
+                      ],
+                    )),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Container(
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value:
+                          (widget.dificuldade>0)?
+                            (nivel/widget.dificuldade) / 10 :1,
+                        ),
                         width: 200,
-                        child: Text(
-                          nome,
-                          style: TextStyle(
-                              fontSize: 24,
-                              overflow: TextOverflow.ellipsis),
-                          )
+                      ),
                     ),
-                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Nível: $nivel",
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ),
                   ],
-                )
+                ),
+              ],
             )
           ],
         ),
---
+++ lib/my_first_widget.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueGrey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.pink, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(color: Colors.redAccent,width: 120,height: 120),
+                  Container(color: Colors.white, width: 110,height: 110),
+                  Container(color: Colors.green, width: 100,height: 100),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 200,
+                child: Text(
+                  "Ola, Mundo",
+                  style: TextStyle(color:Colors.deepPurple,fontSize: 29),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(onPressed: (){print("Voce apertou algo");}, child: Text("Aperta Isso"))
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
